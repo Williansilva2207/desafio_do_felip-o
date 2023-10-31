e1327ac01559fc796833de806617000d8ab0168c
Williansilva2207 <149281430+Williansilva2207@users.noreply.github.com>
2023-10-31 19:03:30 -0300

    Update trabalhodofelipão.js

--- trabalhodofelipão.js
+++ trabalhodofelipão.js
@@ -1,40 +1,43 @@
 const nameHero = "Felipão"
-let xpHero = "900"
+let xpHero = 900
+let xpAcumulador = xpHero
 let nivelHero = ["Ferro","Bronze","Prata","Ouro","Platina","Ascendente","Imortal","Radiante"]
 
 function mensagemDeSaida(xpHero){
     console.log("O Herói " + nameHero, "está no nível " + nivelHero[xpHero - 1])
 }
-
-if(xpHero <= 1000){
-    xpHero = 1
-    mensagemDeSaida(xpHero)
-}
-else if(xpHero > 1000 && xpHero <= 2000){
-    xpHero = 2
-    mensagemDeSaida(xpHero)
-}
-else if(xpHero > 2000 && xpHero <= 6000){
-    xpHero = 3
-    mensagemDeSaida(xpHero)
-}
-else if(xpHero > 6000 && xpHero <= 7000){
-    xpHero = 4
-    mensagemDeSaida(xpHero)
-}
-else if(xpHero > 7000 && xpHero <= 8000){
-    xpHero = 5
-    mensagemDeSaida(xpHero)
-}
-else if(xpHero > 8000 && xpHero <= 9000){
-    xpHero = 6
-    mensagemDeSaida(xpHero)
-}
-else if(xpHero > 9000 && xpHero <= 10000){
-    xpHero = 7
-    mensagemDeSaida(xpHero)
-}
-else{
-    xpHero = 8
-    mensagemDeSaida(xpHero)
+while(xpAcumulador < 10002 ){
+    if(xpAcumulador <= 1000){
+        xpHero = 1
+        mensagemDeSaida(xpHero)
+    }
+    else if(xpAcumulador > 1000 && xpAcumulador <= 2000){
+        xpHero = 2
+        mensagemDeSaida(xpHero)
+    }
+    else if(xpAcumulador > 2000 && xpAcumulador <= 6000){
+        xpHero = 3
+        mensagemDeSaida(xpHero)
+    }
+    else if(xpAcumulador > 6000 && xpAcumulador <= 7000){
+        xpHero = 4
+        mensagemDeSaida(xpHero)
+    }
+    else if(xpAcumulador > 7000 && xpAcumulador <= 8000){
+        xpHero = 5
+        mensagemDeSaida(xpHero)
+    }
+    else if(xpAcumulador > 8000 && xpAcumulador <= 9000){
+        xpHero = 6
+        mensagemDeSaida(xpHero)
+    }
+    else if(xpAcumulador > 9000 && xpAcumulador<= 10000){
+        xpHero = 7
+        mensagemDeSaida(xpHero)
+    }
+    else{
+        xpHero = 8
+        mensagemDeSaida(xpHero)
+    }
+    xpAcumulador += 1000
 }
